c048c6b5f3cd8e228d0bae1eb095dabd232836b2
Zhuang <Cathy.Zhuang@emc.com>
2018-05-25 18:16:31 +0800

    add doc

--- miao .react.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-class miao extends Component {
-
-}
-
-export default ;
--- miaomiaomiao .react.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-class miaomiaomiao extends Component {
-
-}
-
-export default miaomiaomiao;
